782acfda462ee8f8a59916faa7eabc6a1be552ba
martha nakkintu <marthanakintu95@gmail.com>
2025-02-19 08:02:54 +0300

    javascript

---
+++ dataType.js
@@ -0,0 +1,27 @@
+//declare 
+//first name,county,village ,religion
+
+//declare
+//last name,country,parent,bestfriend
+//assign 
+let lastname ="nakintu";
+let country ="uganda";
+let parent ="florence";
+let bestfriend ="musimenta";
+
+//declare
+//first name,last name,marital status,country,age
+
+//first name
+//last name
+//marital status
+//country 
+//age
+
+//assign e.g number of variables
+let  myAge=("I am 25 years old");
+let  yourAge=("You are 30 years old");
+
+console.log(myAge)
+
+console.log(yourAge)   
---
+++ exercise/variables 2.js
@@ -0,0 +1,5 @@
+//declare
+//first name,last name ,country,city
+//boolean e.g isMarried
+//undefined e.gx or y
+//null data e.g 
